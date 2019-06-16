cc8da44db5e95dc2a98d05ccbfb69cc378109785
Christopher McCormack <mccormack.christopher@gmail.com>
2019-06-16 09:26:52 -0700

    feat: changed to simple list

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,61 @@
 import React from "react";
 import styled from 'styled-components'
 
-import Table from "./components/table/Table";
-import TableHead from "./components/table/TableHead";
-import TableBody from "./components/table/TableBody";
-import TableRow from "./components/table/TableRow";
-import TableCell from "./components/table/TableCell";
+// import Table from "./components/table/Table";
+// import TableHead from "./components/table/TableHead";
+// import TableBody from "./components/table/TableBody";
+// import TableRow from "./components/table/TableRow";
+// import TableCell from "./components/table/TableCell";
 
 const Title = styled('h1')`
   text-align: center;
 `
 
-const buildRow = data => (
-  <TableRow>
-    <TableCell>{data.name}</TableCell>
-    {
-      data.values.map(item => <TableCell key={item}>{item}</TableCell>)
-    }
-  </TableRow>
-)
+// const buildRow = data => (
+//   <TableRow>
+//     <TableCell>{data.name}</TableCell>
+//     {
+//       data.values.map(item => <TableCell key={item}>{item}</TableCell>)
+//     }
+//   </TableRow>
+// )
+
+const List = styled('ul')`
+  list-style: none;
+`
+
+const ListItem = styled('li')`
+  list-style-type: none;
+  margin: 1em;
+
+  &.expando:before {
+    content: "▶";
+    display: block;
+    height: 0;
+    width: 0;
+    left: -1.2em;
+    position: relative;
+  }
+`
 
 const buildTree = (data) => {
 
-  if (data.name === 'root') {
-    return;
+  if (data.children.length === 0) {
+    return <ListItem key={data.name}>
+      {`Name: ${data.name}, Values: ${data.values.map(value => value).join(', ')}`}
+    </ListItem>
   }
 
-  if (data.children.length === 0) {
-    return buildRow(data)
-  }
+  const row = data.name !== 'root' &&
+    <ListItem className="expando">
+      {`${data.name} - ${data.level}, Values: ${data.values.map(value=>value).join(', ')}`}
+    </ListItem>
 
   return (
-    <TableBody key={data.name + data.level}>
-      {buildRow(data)}
+    <List key={data.name}>
+      {row}
       {data.children.map(item => buildTree(item))}
-    </TableBody>
+    </List>
   )
 
 }
@@ -42,13 +63,10 @@ const buildTree = (data) => {
 const App = ({ data }) => 
     <div className="App">
       <Title>{"Tara's Cool Table Thingy"}</Title>
-      <Table className="table">
-        <TableHead
-          className="table-head"
-          values={data.values.map(value => value)}
-        />
+      <List>
+      <ListItem>{`${data.values.map(value => value).join(', ')}`}</ListItem>
         { buildTree(data) }
-      </Table>
+      </List>
     </div>
 
 export default App
